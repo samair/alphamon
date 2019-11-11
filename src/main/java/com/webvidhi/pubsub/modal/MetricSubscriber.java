7d044ae34be66376b78dd9644a59b3b1030b32aa
samair <tsameerc@gmail.com>
2019-11-11 20:50:54 +0530

    Websocket Session handling

--- src/main/java/com/webvidhi/pubsub/modal/MetricSubscriber.java
+++ src/main/java/com/webvidhi/pubsub/modal/MetricSubscriber.java
@@ -1,6 +1,7 @@
 package com.webvidhi.pubsub.modal;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -8,13 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class MetricSubscriber {
 
-	private List<String> subscriberList = new ArrayList<>();
+	private List<String> subscriberList = Collections.synchronizedList(new  ArrayList<>());
 	
 	public void addSubscriber(String deviceId) {
+		System.out.println("Adding subscriber-"+deviceId);
 		subscriberList.add(deviceId);
 	}
 	
 	public void removeSubscriber(String deviceId) {
+		System.out.println("Removing  subscriber-"+deviceId);
 		subscriberList.remove(deviceId);
 	}
 	
