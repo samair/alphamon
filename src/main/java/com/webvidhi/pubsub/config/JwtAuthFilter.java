78aa27afb566f6acd165675d51843c49c14baf30
samair <tsameerc@gmail.com>
2019-12-31 12:06:13 +0530

    SockJS

--- src/main/java/com/webvidhi/pubsub/config/JwtAuthFilter.java
+++ src/main/java/com/webvidhi/pubsub/config/JwtAuthFilter.java
@@ -77,7 +77,8 @@ System.out.println("Error!");
 	    		
 	    	}
 	    }
-	    response.setHeader("Access-Control-Allow-Origin", "*");
+	    
+	    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
